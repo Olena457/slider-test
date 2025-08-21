5d950b918aed742b6ef60ca69c8b61a0e2ac3f54
Olena457 <olenademianenko42@gmail.com>
2025-08-21 23:26:12 +0300

    created tools component

---
+++ src/componets/Header/Header.jsx
@@ -0,0 +1,7 @@
+import css from './Header.module.css';
+
+const Header = () => {
+  return <div className={css.header}></div>;
+};
+
+export default Header;
---
+++ src/componets/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import css from './Layout.module.css';
+
+const Layout = () => {
+  return (
+    <div className={css.layout}>
+      <main className={css.main}>Main Content</main>
+    </div>
+  );
+};
+
+export default Layout;
